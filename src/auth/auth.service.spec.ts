4b7a6a82c8c346632959419c59d1fe4597beffdf
Gabriel Anacleto <gabrielanacleto159@live.com>
2024-01-29 20:07:55 -0300

    :boom: fix: changes in bank settings

--- src/auth/auth.service.spec.ts
+++ src/auth/auth.service.spec.ts
@@ -9,9 +9,12 @@ import {
   UserRepositoryMock,
   GetFilesMock,
 } from '../testing';
-import { HttpException, UnauthorizedException, BadRequestException } from '@nestjs/common';
-import * as bcrypt from 'bcrypt'
-
+import {
+  HttpException,
+  UnauthorizedException,
+  BadRequestException,
+} from '@nestjs/common';
+import * as bcrypt from 'bcrypt';
 
 describe('Auth Service', () => {
   let Auth_Service: AuthService;
@@ -30,62 +33,55 @@ describe('Auth Service', () => {
 
   describe('Create', () => {
     it('Should register a new user.', async () => {
-        const Photo = await GetFilesMock();
-   
+      const Photo = await GetFilesMock();
 
-      const NewUser = await Auth_Service.RegisterAuthService(CreateUserMock, Photo);
-  
+      const NewUser = await Auth_Service.RegisterAuthService(
+        CreateUserMock,
+        Photo,
+      );
+
       expect(NewUser).toEqual(ListUsersMock[0]);
     });
 
-    
     it('Should not allow creating a user with the same email', async () => {
       const Photo = await GetFilesMock();
-    
+
       jest.spyOn(UserRepositoryMock.useValue, 'exists').mockResolvedValue(true);
-    
+
       const result = Auth_Service.RegisterAuthService(CreateUserMock, Photo);
-    
+
       await expect(result).rejects.toThrow(HttpException);
     });
-    
+  });
 
-
-  })
-
-
-
-
-  describe("Auth login", () => {
-    it("Should return a token if email and password are correct", async () => {
-
-      jest.spyOn(bcrypt, 'compare').mockResolvedValueOnce(true as never)
+  describe('Auth login', () => {
+    it('Should return a token if email and password are correct', async () => {
+      jest.spyOn(bcrypt, 'compare').mockResolvedValueOnce(true as never);
 
       const LoginAuth = await Auth_Service.LoginAuthService(LoginMock);
 
-      expect(LoginAuth).toEqual({token: LoginReponseMock})
-    })
-
-    it("should return an exception when the user is not found", async () => {
-      jest.spyOn(UserRepositoryMock.useValue, 'findOne').mockResolvedValue(null);
-    
-      const loginAuthPromise = Auth_Service.LoginAuthService(LoginMock);
-    
-      await expect(loginAuthPromise).rejects.toThrow(HttpException);
+      expect(LoginAuth).toEqual({ token: LoginReponseMock });
     });
-    
-    
-    it("should return an exception when the user is not found", async () => {
-      jest.spyOn(UserRepositoryMock.useValue, 'findOne').mockResolvedValue(true);
-      jest.spyOn(bcrypt, 'compare').mockResolvedValueOnce(false as never)
+
+    it('should return an exception when the user is not found', async () => {
+      jest
+        .spyOn(UserRepositoryMock.useValue, 'findOne')
+        .mockResolvedValue(null);
 
       const loginAuthPromise = Auth_Service.LoginAuthService(LoginMock);
-    
+
       await expect(loginAuthPromise).rejects.toThrow(HttpException);
     });
 
+    it('should return an exception when the user is not found', async () => {
+      jest
+        .spyOn(UserRepositoryMock.useValue, 'findOne')
+        .mockResolvedValue(true);
+      jest.spyOn(bcrypt, 'compare').mockResolvedValueOnce(false as never);
 
+      const loginAuthPromise = Auth_Service.LoginAuthService(LoginMock);
 
-  })
-
-})
+      await expect(loginAuthPromise).rejects.toThrow(HttpException);
+    });
+  });
+});
---
+++ src/testing/produtcts/index.ts
@@ -0,0 +1,2 @@
+export * from './product.mock'
+export * from './products-repository.mock'
