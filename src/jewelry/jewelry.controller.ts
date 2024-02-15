28ac0939b813a8dd2ff1d484288206e4aa87dbcd
syus13 <syus13@gmail.com>
2024-02-14 22:58:16 -0300

    fix: swagger jewelry

--- src/jewelry/jewelry.controller.ts
+++ src/jewelry/jewelry.controller.ts
@@ -9,6 +9,8 @@ import {
   Delete,
   UseGuards,
   UploadedFile,
+  HttpStatus,
+  HttpCode,
 } from '@nestjs/common';
 import { JewelryService } from './jewelry.service';
 import { CreateJewelryDto } from './dto/create-jewelry.dto';
@@ -19,13 +21,29 @@ import { RolesGuards } from '../auth/guards/role-guard';
 import { RoleEnum } from '../enums/role.enum';
 import { FileDTO } from '../auth/dto/files.dto';
 import { FileInterceptor } from '@nestjs/platform-express';
+import { ApiBearerAuth, ApiBody, ApiResponse, ApiTags } from '@nestjs/swagger';
+import { CreateJewelryDoc } from './docs/create-jewelry.doc';
+import { CreatedJewelryDoc } from './docs/created-jewelry.doc';
+import { GiveJewelryDoc } from './docs/give-jewelry.doc';
+import { UpdateJewelryDoc } from './docs/update-jewelry.doc';
+import { DeleteJewelryResponseDoc } from './docs/delete-jewelry-response.doc';
+import { CreatedUserDoc } from 'src/auth/docs/createdUser.doc';
 
+@ApiTags('Jewelry')
+@ApiBearerAuth()
 @Controller('jewelry')
 export class JewelryController {
   constructor(private readonly jewelryService: JewelryService) {}
 
   @UseGuards(AuthGuard, RolesGuards)
   @Roles(RoleEnum.admin)
+  @ApiBody({
+    type: CreateJewelryDoc,
+  })
+  @ApiResponse({
+    status: HttpStatus.CREATED,
+    type: CreatedJewelryDoc,
+  })
   @Post()
   @UseInterceptors(FileInterceptor('image'))
   create(
@@ -36,6 +54,11 @@ export class JewelryController {
   }
 
   @UseGuards(AuthGuard, RolesGuards)
+  @ApiResponse({
+    type: CreatedJewelryDoc,
+    isArray: true,
+  })
+  @Roles(RoleEnum.admin)
   @Get()
   findAll() {
     return this.jewelryService.FindAllJewelry();
@@ -43,6 +66,13 @@ export class JewelryController {
 
   @UseGuards(AuthGuard, RolesGuards)
   @Roles(RoleEnum.admin)
+  @ApiBody({
+    type: GiveJewelryDoc,
+  })
+  @ApiResponse({
+    status: HttpStatus.OK,
+    type: CreatedJewelryDoc,
+  })
   @Post(':userId/:jewelryId')
   GiveJewelry(
     @Param('userId') userId: string,
@@ -52,6 +82,10 @@ export class JewelryController {
   }
 
   @UseGuards(AuthGuard, RolesGuards)
+  @ApiResponse({
+    status: HttpStatus.OK,
+    type: CreatedUserDoc,
+  })
   @Get(':id')
   findOne(@Param('id') id: string) {
     return this.jewelryService.findOne(+id);
@@ -59,11 +93,23 @@ export class JewelryController {
 
   @UseGuards(AuthGuard, RolesGuards)
   @Roles(RoleEnum.admin)
+  @ApiBody({
+    type: UpdateJewelryDoc,
+  })
+  @ApiResponse({
+    status: HttpStatus.OK,
+    type: CreatedJewelryDoc,
+  })
   @Patch(':id')
   update(@Param('id') id: string, @Body() updateJewelryDto: UpdateJewelryDto) {
     return this.jewelryService.Update(+id, updateJewelryDto);
   }
 
+  @HttpCode(HttpStatus.ACCEPTED)
+  @ApiResponse({
+    status: HttpStatus.ACCEPTED,
+    type: DeleteJewelryResponseDoc,
+  })
   @Delete(':id')
   remove(@Param('id') id: string) {
     return this.jewelryService.Remove(+id);
