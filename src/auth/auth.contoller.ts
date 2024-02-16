a4ece948916f20cf9480a8ffbe901760c9d7f795
syus13 <syus13@gmail.com>
2024-02-15 21:29:46 -0300

    feat: docs

--- src/auth/auth.contoller.ts
+++ src/auth/auth.contoller.ts
@@ -5,7 +5,6 @@ import {
   HttpStatus,
   Post,
   UploadedFile,
-  UseGuards,
   UseInterceptors,
 } from '@nestjs/common';
 // import { ApiBody, ApiResponse, ApiTags, ApiBearerAuth } from '@nestjs/swagger';
@@ -17,10 +16,6 @@ import { LoginDTO } from './dto/login.dto';
 import { ApiBearerAuth, ApiBody, ApiResponse, ApiTags } from '@nestjs/swagger';
 import { LoginDoc } from './docs/login.doc';
 import { LoginResponseDoc } from './docs/loginResponse.doc';
-import { AuthGuard } from './guards/auth-guard';
-import { RolesGuards } from './guards/role-guard';
-import { Roles } from './decorators/roles';
-import { RoleEnum } from 'src/enums/role.enum';
 import { UserRegisterDoc } from './docs/register.doc';
 import { CreatedUserDoc } from './docs/createdUser.doc';
 
@@ -28,8 +23,6 @@ import { CreatedUserDoc } from './docs/createdUser.doc';
 @Controller('auth')
 export class AuthController {
   constructor(private authService: AuthService) {}
-  @UseGuards(AuthGuard, RolesGuards)
-  @Roles(RoleEnum.admin)
   @ApiBody({
     type: UserRegisterDoc,
   })
